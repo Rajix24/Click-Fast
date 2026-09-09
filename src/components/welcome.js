750051bbecf83f34e92eff2f1d64f47d6272bea3
Younes Raji <rajix2100@gmail.com>
2026-09-09 11:51:31 +0100

    create board ui

--- src/components/welcome.js
+++ src/components/welcome.js
@@ -3,6 +3,6 @@ import { navigate } from "../routes/route.js"
 export default function Welcome(){
     const homeCompoments = document.createElement('div')
     homeCompoments.className = "home-conponent"
-    homeCompoments.appendChild(button({class: "homeBTN",  id:"homeBTN", content:"Get started"}, navigate, "/board"))
+    homeCompoments.appendChild(button({class: "homeBTN",  id:"homeBTN", content:"jump to board"}, navigate, "/board"))
     return homeCompoments
 }
--- src/pages/Board.js
+++ src/pages/Board.js
@@ -1,5 +1,16 @@
-export default function Board(){
-    const board = document.createElement("p")
-    board.innerText = "board pages";
+import BoardComponents from "../components/board.js";
+export default function Board(BoardCounter = 12){
+    let rows = 2;
+    let columns = 3;
+    if (BoardCounter == 9) {rows = 3};
+    if(BoardCounter == 16) {rows = columns = 4} 
+    if(BoardCounter == 12) {rows =3 ;  columns = 4} 
+    const style = `grid-template-columns: repeat(${columns}, 1fr); grid-template-rows: repeat(${rows}, 1fr); backgound: red`
+    const board = document.createElement("div")
+    board.className = "container board"
+    board.style = style
+    for (let i = 0; i < BoardCounter; i++) {
+        board.appendChild(BoardComponents()) 
+    }
     return board;
 }
