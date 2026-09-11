0540e6b2c5253ff6906d6fb953f7fea62e6696b8
Younes Raji <rajix2100@gmail.com>
2026-09-11 17:03:54 +0100

    change structure of code and add function start time the stop it

--- src/components/ActiveBar.js
+++ src/components/ActiveBar.js
@@ -1,17 +1,3 @@
 import  ResultComponents  from "./sousComponents/ResultComponent.js"
 import TimeComponent from "./sousComponents/TimerComponents.js"
 
-export default function ActiveBar(){
-    const bar = document.createElement("div")
-
-
-    bar.className = "bar"
-
-
-    bar.appendChild(TimeComponent())
-    bar.appendChild(ResultComponents())
-
-
-
-    return bar;
-}
